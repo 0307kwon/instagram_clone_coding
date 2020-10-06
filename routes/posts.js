4829d79ca5a96e570bacc9ee39d7ecf79bb2b85b
권세진 <0307kwon@gmail.com>
2020-10-07 01:05:51 +0900

    이미지 업로드 오류 수정

--- routes/posts.js
+++ routes/posts.js
@@ -12,22 +12,28 @@ const storage = multer.diskStorage({
       cb(null, './public/images');
     },
     filename: function (req, file, cb) {
-        const revision_title = req.body.title;
-        const contents = req.body.contents;
-        req.post_changed = {
-            title:revision_title,
-            contents:contents,
-            time:new Date(),
-        };
-
-        if(req.post.filename_image === "" ){ // 이전 post작성에 사진을 저장하지 않았으면
+        if(req.post === undefined){ //create의 경우
             const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
             const extention = path.extname(file.originalname);
             const filename_image = file.fieldname + '-' + uniqueSuffix+extention;
             cb(null, filename_image);
-            req.post_changed.filename_image = filename_image;
-        }else{ //
-            cb(null, req.post.filename_image);
+        }else{ // update의 경우
+            const revision_title = req.body.title;
+            const contents = req.body.contents;
+            req.post_changed = {
+                title:revision_title,
+                contents:contents,
+                time:new Date(),
+            };
+            if(req.post.filename_image === ""){ // 이전에 이미지를 저장하지 않은경우
+                const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+                const extention = path.extname(file.originalname);
+                const filename_image = file.fieldname + '-' + uniqueSuffix+extention;
+                cb(null, filename_image);
+                req.post_changed.filename_image = filename_image;
+            }else{ // 이전에 이미지를 저장한 경우 (이미지 업데이트)
+                cb(null, req.post.filename_image);
+            }
         }
     }
   });
