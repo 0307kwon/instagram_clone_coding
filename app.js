67de7f90e1710aefe403b734a08c246e344258ab
권세진 <0307kwon@gmail.com>
2020-10-31 21:09:53 +0900

    메인페이지 완성

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
+const loginRouter = require('./routes/login/login');
 var usersRouter = require('./routes/users');
 const postsRouter = require('./routes/posts/posts');
 
@@ -21,6 +22,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/login',loginRouter);
 app.use('/posts',postsRouter);
 app.use('/users', usersRouter);
   
